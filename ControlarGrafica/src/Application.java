859ca27157e0d6273c293b6d648ca69de5f80dc3
Ravi Assis <32408043+raviassis@users.noreply.github.com>
2018-09-19 13:39:47 -0300

    Add files via upload

--- ControlarGrafica/src/Application.java
+++ ControlarGrafica/src/Application.java
@@ -8,9 +8,11 @@ public class Application {
 		Arquivo a = new Arquivo();
 		List<Pedido> lista = a.ler("dadosGrafica.txt");
 		
-		for(Pedido p : lista){
+		/*for(Pedido p : lista){
 			System.out.println(p);
-		}
+		}*/
+		
+		lista.stream().sorted( (x,y) -> x.getPrioridade() - y.getPrioridade()).forEach(System.out::println);
 	}
 
 }
