a9a787af75457811b8c36bd15833523dc4bee61d
Ravi Assis <ravi.assis@dtidigital.com.br>
2018-09-29 18:43:19 -0300

    Alterações na impressora
    Necessário ainda melhorar o método run

--- ControlarGrafica/src/Impressora.java
+++ ControlarGrafica/src/Impressora.java
@@ -2,6 +2,14 @@
 public class Impressora extends Thread{
 	
 	private Pedido servico;
+	
+	private Gerente gerente;
+	private int regra;
+	
+	public Impressora (Gerente g, int regra) {
+		this.gerente = g;
+		this.regra = regra;
+	}
 		
 	public Pedido getServico() {
 		return servico;
@@ -14,10 +22,19 @@ public class Impressora extends Thread{
 	@Override
 	public void run() {
 
-		int pgImpri = servico.getNumPag(); 
+		/*int pgImpri = servico.getNumPag(); 
 		for(int i = 80;i < pgImpri;i+=80) {
 			pgImpri-=80;
+		}*/
+		
+		if(this.regra == 1) {
+			System.out.println( gerente.getPedidoMaiorPrioridade());
 		}
+		else {
+			System.out.println(this.gerente.getPedidoMenorTrabalho());
+		}
+		
+		
 
 	}
 }
